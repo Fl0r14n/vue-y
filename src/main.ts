44813413bda8eed32b6a5f213d4003c45b61a882
Florian Chis <florian.chis@gmail.com>
2023-01-19 11:43:41 +0200

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/main.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(
+  createVuetify({
+    theme: {
+      themes: {
+        light: {
+          colors: {
+            primary: '#1867C0',
+            secondary: '#5CBBF6'
+          }
+        }
+      }
+    }
+  })
+)
 
 app.mount('#app')
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
+import viteBasicSslPlugin from '@vitejs/plugin-basic-ssl'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    viteBasicSslPlugin(),
+    vue({
+      template: { transformAssetUrls }
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/assets/variables.scss'
+      }
+    })
+  ],
+  define: { 'process.env': {} },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 4200,
+    strictPort: true,
+    https: true
   }
 })
