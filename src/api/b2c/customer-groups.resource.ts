6507d5a23e058e53e99c2344050ae55b2247679c
Florian Chis <florian.chis@gmail.com>
2023-03-27 12:45:37 +0300

    more rest migration

--- src/api/b2c/customer-groups.resource.ts
+++ src/api/b2c/customer-groups.resource.ts
@@ -1,52 +1,26 @@
 import type { MemberListData, PageableRequestData, RequestData, UserGroupData, UserGroupListData } from '@/api/models'
-import { RestClient } from '@/api/rest'
+import { useRestClient, useRestContext } from '@/api/rest'
+import { inject } from '@/config'
+import { computed } from 'vue'
 
-export class CustomerGroupsResource extends RestClient {
-  getEndpoint() {
-    return `${this.basePath}/customergroups`
-  }
+export abstract class CustomerGroupsResource {
+  getCustomerGroups?: (queryParams?: PageableRequestData) => Promise<UserGroupListData>
+  getCustomerGroup?: (groupId: string, queryParams?: RequestData) => Promise<UserGroupData>
+  addCustomerGroup?: (userGroup: UserGroupData) => Promise<void>
+  addCustomerGroupUser?: (groupId: string, members: MemberListData) => Promise<void>
+  delCustomerGroupUser?: (groupId: string, userId: string) => Promise<void>
+}
 
-  /**
-   * Returns all customer groups that are direct subgroups of a customergroup.
-   * To try out the methods of the Customer Groups controller, you must authorize a user who belongs to the “customermanagergroup”.
-   * @param {PageableRequestData} queryParams
-   */
-  getCustomerGroups(queryParams?: PageableRequestData) {
-    return this.query<UserGroupListData>({ params: queryParams })
-  }
-
-  /**
-   * Returns a customer group with a specific groupId.
-   * @param {string} groupId. Group identifier
-   * @param {RequestData} queryParams
-   */
-  getCustomerGroup(groupId: string, queryParams?: RequestData) {
-    return this.get<UserGroupData>(groupId, { params: queryParams })
-  }
-
-  /**
-   * Creates a new customer group that is a direct subgroup of a customergroup.
-   * @param {UserGroupData} userGroup.
-   */
-  addCustomerGroup(userGroup: UserGroupData) {
-    return this.post<void>(userGroup)
-  }
-
-  /**
-   * Assigns user(s) to a customer group.
-   * @param {string} groupId. Group identifier
-   * @param {MemberListData} members
-   */
-  addCustomerGroupUser(groupId: string, members: MemberListData) {
-    return this.patch<void>(`${groupId}/members`, members)
-  }
-
-  /**
-   * Removes user from a customer group.
-   * @param {string} groupId. Group identifier
-   * @param {string} userId. User identifier
-   */
-  delCustomerGroupUser(groupId: string, userId: string) {
-    return this.delete<void>(`${groupId}/members/${userId}`)
+const customerGroupsResource = (): CustomerGroupsResource => {
+  const { sitePath } = useRestContext()
+  const rest = useRestClient(computed(() => `${sitePath.value}/customergroups`))
+  return {
+    getCustomerGroups: (queryParams?: PageableRequestData) => rest.query<UserGroupListData>({ params: queryParams }),
+    getCustomerGroup: (groupId: string, queryParams?: RequestData) => rest.get<UserGroupData>(groupId, { params: queryParams }),
+    addCustomerGroup: (userGroup: UserGroupData) => rest.post<void>(userGroup),
+    addCustomerGroupUser: (groupId: string, members: MemberListData) => rest.patch<void>(`${groupId}/members`, members),
+    delCustomerGroupUser: (groupId: string, userId: string) => rest.del<void>(`${groupId}/members/${userId}`)
   }
 }
+
+export const useCustomerGroupsResource = () => inject(CustomerGroupsResource, customerGroupsResource())
--- src/api/b2c/export.resource.ts
+++ src/api/b2c/export.resource.ts
@@ -1,33 +1,30 @@
 import type { PageableRequestData, ProductListData } from '@/api/models'
-import { RestClient } from '@/api/rest'
+import { useRestClient, useRestContext } from '@/api/rest'
+import { inject } from '@/config'
+import { computed } from 'vue'
 
-export class ExportResource extends RestClient {
-  getEndpoint() {
-    return `${this.basePath}/export`
-  }
-
-  /**
-   * Used for product export.
-   * Depending on the timestamp parameter, it can return all products or only products modified after the given time.
-   * @param {{catalog?: string; version?: string; timestamp?: Date} & PageableRequestData} queryParams
-   */
-  getProducts(
+export abstract class ExportResource {
+  getProducts?: (
     queryParams?: {
-      /**
-       * Catalog from which get products. Must be provided along with version.
-       */
       catalog?: string
-      /**
-       * Catalog version. Must be provided along with catalog.
-       */
       version?: string
-      /**
-       * When this parameter is set, only products modified after given time will be returned.
-       * This parameter should be in RFC-8601 format.
-       */
       timestamp?: Date
     } & PageableRequestData
-  ) {
-    return this.get<ProductListData>(`products`, { params: queryParams })
+  ) => Promise<ProductListData>
+}
+
+const exportResource = (): ExportResource => {
+  const { sitePath } = useRestContext()
+  const rest = useRestClient(computed(() => `${sitePath.value}/export`))
+  return {
+    getProducts: (
+      queryParams?: {
+        catalog?: string
+        version?: string
+        timestamp?: Date
+      } & PageableRequestData
+    ) => rest.get<ProductListData>(`products`, { params: queryParams })
   }
 }
+
+export const useExportResource = () => inject(ExportResource, exportResource())
